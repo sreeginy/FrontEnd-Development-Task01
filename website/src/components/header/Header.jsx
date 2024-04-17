936d1472d833fe3400518ea0f1d2868595f9f272
Sreeginy <sreeginy98@gmail.com>
2024-04-17 22:43:34 +0530

    create Header

---
+++ website/src/components/header/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './header.css';
+import '../../index.css'
+import Logo from "../../assets/coffelogo01.png";
+
+
+const Header = () => {
+  return (
+    <nav className="container">
+
+       <img className="logo" src={Logo} alt=""/> 
+
+       <button className="button"> Contact </button>   
+       
+   </nav> 
+  )
+}
+
+export default Header
+
