21173bb270f0ea5f3400245d4cbb7cd835774575
Sreeginy <sreeginy98@gmail.com>
2024-04-20 18:18:29 +0530

    update product css

--- website/src/components/Products/Products.jsx
+++ website/src/components/Products/Products.jsx
@@ -6,7 +6,6 @@ import Product02 from "../../assets/product002.jpg";
 import Product03 from "../../assets/product03.jpg";
 
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
@@ -17,49 +16,71 @@ const Products = () => {
     <div className="title"> 
     <h1>OUR PRODUCTS</h1>
     <div className='products container'>
-        <div className="product">
 
-        <Card sx={{ maxWidth: 345 }}>
+            <div className="card-container">
+              
+    <Card sx={{ maxWidth: 345 }} className='textstyle'>
       <CardMedia
         component="img"
         alt="coffee"
-        height="350"
+        height="300"
         image={Product01}
       />
-      <CardContent>
+      <CardContent >
         <Typography gutterBottom variant="h5" component="div">
         Original Coffee
         </Typography>
-        <Typography variant="body2" color="text.secondary" style={{ alignItems: "left"}}>
-        A coffee café is an establishment that primarily serves various types of coffee, espresso, latte.
-        </Typography>
+        <Typography variant="body1" color="text.secondary" >
+        A coffee café is an establishment that serves various types of coffee, espresso, latte.
+        </Typography> <br />
+        <Button variant="contained" size="medium" >View More</Button> <br />
       </CardContent>
-      <CardActions>
-        <Button size="small" color="secondary">View More</Button>
-      </CardActions>
     </Card>
 
-            {/* <img src={Product01} alt="" />
-            <h3>Original Coffee</h3>
-            <p>A coffee café is an establishment that primarily serves various types of coffee, espresso, latte.</p>
-            <button className='button3'>Explore More</button>  */}
-        </div> 
+   <Card sx={{ maxWidth: 345 }} className='textstyle'>
+      <CardMedia
+        component="img"
+        alt="coffee"
+        height="300"
+        image={Product02}
+      />
+      <CardContent >
+        <Typography gutterBottom variant="h5" component="div">
+        20 Coffee Flavors
+        </Typography>
+        <Typography variant="body1" color="text.secondary" >
+        A coffee café is an establishment that serves various types of coffee, espresso, latte.
+        </Typography> <br />
+        <Button variant="contained" size="medium" >View More</Button> <br />
+      </CardContent>
+    </Card>      
 
-        <div className="product">
-            <img src={Product02} alt="" />
-            <h3>20 Coffee Flavors</h3>
-            <p>A coffee café is an establishment that primarily serves various types of coffee, espresso, latte.</p>
-            <button className='button3'>Explore More</button>
-        </div>
 
-        <div className="product">
-            <img src={Product03} alt="" />
-            <h3>Signature Coffee</h3>
-            <p>A coffee café is an establishment that primarily serves various types of coffee, espresso, latte.</p>
-            <button className='button3'>Explore More</button>
-        </div>
+
+    <Card sx={{ maxWidth: 345 }} className='textstyle'>
+      <CardMedia
+        component="img"
+        alt="coffee"
+        height="300"
+        image={Product03}
+      />
+      <CardContent >
+        <Typography gutterBottom variant="h5" component="div">
+        Signature Coffee
+        </Typography>
+        <Typography variant="body1" color="text.secondary" >
+        A coffee café is an establishment that serves various types of coffee, espresso, latte.
+        </Typography> <br />
+        <Button variant="contained" size="medium" >View More</Button> <br />
+      </CardContent>
+    </Card>   
+
     </div>
-    </div>
+  </div> 
+
+  </div>
+
+
   )
 }
 
